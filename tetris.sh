e249f045ddb76591704f185e3f2c5f800cb54c96
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-04-18 17:07:30 +0800

    tetrish.sh: Update command for multithreading

--- tetris.sh
+++ tetris.sh
@@ -1,4 +1,6 @@
-cd /home/e/e0215813/canttouchthis/src/
-javac -cp '.:jswarm-pso_2_08.jar' *.java
-java -cp '.:jswarm-pso_2_08.jar' PSO 30 100
+cd /home/e/e0215813/canttouchthismulti/src/
+# javac -cp '.:jswarm-pso_2_08.jar' *.java
+# java -cp '.:jswarm-pso_2_08.jar' PSO 30 100
+javac *.java jswarm_pso/*.java
+java PSO 30 300 10
 
